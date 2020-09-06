e25bf49058bc3aa41948685343d940c3f73fdaf6
Nikhil I <66690978+ironnicko@users.noreply.github.com>
2020-09-06 21:35:13 +0530

    Update and rename Daynote.py to youtube_video_name_graber.py

--- youtube_video_name_graber.py
+++ youtube_video_name_graber.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 
-URL = 'https://www.youtube.com/watch?v=1WSDn3B_Z_s'
+URL = input('Enter the URL:')
 
 PAGE = requests.get(URL).text
 soup = BeautifulSoup(PAGE,'lxml')
